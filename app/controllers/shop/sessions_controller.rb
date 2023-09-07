b2ca51a3c6703c0a3ae98366245c48ddde387daf
EC2 Default User <ec2-user@ip-172-31-31-207.ap-northeast-1.compute.internal>
2023-09-07 07:07:19 +0000

    break

--- app/controllers/shop/sessions_controller.rb
+++ app/controllers/shop/sessions_controller.rb
@@ -2,7 +2,10 @@
 
 class Shop::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  def after_sign_in_path_for(resource)
+    @shop = current_shop
+    to_gos_path
+  end
   # GET /resource/sign_in
   # def new
   #   super
