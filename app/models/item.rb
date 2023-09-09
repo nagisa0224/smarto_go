9c68587101526004d1a19e487248ab29cc810ae5
EC2 Default User <ec2-user@ip-172-31-31-207.ap-northeast-1.compute.internal>
2023-09-09 09:06:19 +0000

    [Add]items

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,5 +13,12 @@ class Item < ApplicationRecord
         end
     end
     
+    def get_image(width, height)
+        unless image.attached?
+          file_path = Rails.root.join('app/assets/images/no_image.jpg')
+          image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+        end
+        image.variant(resize_to_limit: [width, height]).processed
+    end
     
 end
