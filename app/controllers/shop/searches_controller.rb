d11e880bb98525181b5f3db0caaac624f1df763c
EC2 Default User <ec2-user@ip-172-31-31-207.ap-northeast-1.compute.internal>
2023-09-13 12:34:44 +0000

    debug-edit

--- app/controllers/shop/searches_controller.rb
+++
@@ -1,8 +0,0 @@
-class Shop::SearchesController < ApplicationController
-  
-  #def search
-    #@customers = ToGo.search(params[:search])
-    #@teamsは好きなものを入力して、Adminの部分には検索したいテーブル名を入力する
-  #end
-  
-end
--- test/controllers/shop/searches_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Shop::SearchesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
