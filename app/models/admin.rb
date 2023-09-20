d83938a1457e3e7ca7de78b4db8ec021e49ed8ed
nagisa0224 <lefretshurk@gmail.com>
2023-09-20 17:36:41 +0000

    仮レイアウト

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,14 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  
+  def shop_status
+        if is_admission == true
+            "稼働中"
+        else
+            "退会済"
+        end
+  end
+  
 end
