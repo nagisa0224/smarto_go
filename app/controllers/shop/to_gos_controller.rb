2bc16dfabecf98d324d534481e3c46bf01f26663
nagisa0224 <lefretshurk@gmail.com>
2023-10-15 07:09:58 +0000

    break

--- app/controllers/shop/to_gos_controller.rb
+++ app/controllers/shop/to_gos_controller.rb
@@ -23,7 +23,7 @@ class Shop::ToGosController < ApplicationController
   
   
   def index
-    @to_gos = ToGo.where('date >= ?', Date.current).order(:date).page(params[:page]).per(20)
+    @to_gos = ToGo.where(shop_id: current_shop.id).where('date >= ?', Date.current).order(:date).page(params[:page]).per(20)
     @to_gos_all = ToGo.all
   end
   
@@ -81,6 +81,7 @@ class Shop::ToGosController < ApplicationController
   #検索に関するコントローラー
   def search
     @to_gos = ToGo.search(params[:keyword]).page(params[:page]).per(20)
+    @type = params[:type]
   end
   
   
